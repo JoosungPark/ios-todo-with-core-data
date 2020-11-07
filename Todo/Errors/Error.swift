f96909993a5ccb3961e7782936ae70163b612e6e
joosung.park <sdop25@gmail.com>
2020-11-07 12:52:27 +0900

    CustomError 추가

---
+++ Todo/Errors/Error.swift
@@ -0,0 +1,13 @@
+//
+//  Error.swift
+//  Todo
+//
+//  Created by 박주성 on 11/7/20.
+//  Copyright © 2020 sdop. All rights reserved.
+//
+
+import Foundation
+
+enum TodoError: Error {
+    case itemNotFound
+}
