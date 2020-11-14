954c7f68a9ca1547297a7cfcd9796eeac1674409
joosung.park <sdop25@gmail.com>
2020-11-15 07:31:27 +0900

    TodoTests refactoring

--- TodoTests/TodoTests.swift
+++ TodoTests/TodoTests.swift
@@ -11,6 +11,13 @@ import XCTest
 
 class TodoTests: XCTestCase {
 
+    // // singleton보다 객체 생성 후 사용하는 게 side effect을 없도록 보장하는 측면에서 좋다.
+    private var folderController: FolderController {
+        get {
+            return FolderController()
+        }
+    }
+    
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
     }
@@ -19,17 +26,15 @@ class TodoTests: XCTestCase {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
 
-    func testFolderCRUD() throws {
-        // singleton보다 객체 생성 후 사용하는 게 side effect을 없도록 보장하는 측면에서 좋다.
-        let folderController = FolderController()
+    func testCreateFolder() throws {
         let testFolder1 = "testFolder1"
-        
         let resultTestFolder1: Folder = folderController.createFolder(title: testFolder1)
 
         XCTAssertEqual(resultTestFolder1.title, testFolder1)
         XCTAssertEqual(resultTestFolder1.order, 1)
-        
-        // retrieve
+    }
+    
+    func testReadFolder() throws {
         let testFolderId = 1
         let retrievedFolder: Folder? = try! folderController.retrieveFolder(id: testFolderId)
         
@@ -45,8 +50,9 @@ class TodoTests: XCTestCase {
         } catch {
             XCTFail()
         }
-        
-        // update
+    }
+    
+    func testUpdateFolder() throws {
         let updateFolderId: FolderId = 1
         let updatedTitle = "updated Title"
         let updatedFolder: Folder = try! folderController.updateFolder(id: updateFolderId, title: updatedTitle)
@@ -61,14 +67,12 @@ class TodoTests: XCTestCase {
         } catch {
             XCTFail()
         }
-        
-        
-        // 다음 주에
-        // delete 관련 코드는 제가 작성하고 추후 검수만 하고
-        // Todo도 테스트 코드를 추가해 온다.
-        
-        // 개괄적인 건 작성해올 게용
-        // Todo와 Folder를 같이 테스트하는 건 다음 스터디에서 하자
-        
     }
+    
+    // 다음 주에
+    // delete 관련 코드는 제가 작성하고 추후 검수만 하고
+    // Todo도 테스트 코드를 추가해 온다.
+    
+    // 개괄적인 건 작성해올 게용
+    // Todo와 Folder를 같이 테스트하는 건 다음 스터디에서 하자
 }
